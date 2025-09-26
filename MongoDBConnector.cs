31ebb950c43ddadaed3005fadf6b029642fb4653
atiliaa2343 <atiliaa.thomas@gmail.com>
2025-09-26 16:02:43 -0400

    initialized database

--- MongoDBConnector.cs
+++ MongoDBConnector.cs
@@ -1,7 +1,21 @@
+using MongoDB.Driver; // Make sure you have MongoDB.Driver NuGet package installed
+
 namespace DatabaseConnector
 {
     public class MongoDBConnector
     {
-        // Future connection code here
+        private readonly IMongoDatabase _database; // field to hold the database connection
+
+        public MongoDBConnector(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        // Optional: expose the database so it can be used in other classes
+        public IMongoDatabase GetDatabase()
+        {
+            return _database;
+        }
     }
 }
